39c176e8e8b2699bbd311ed878a5066a0374ec87
Wilkson Flor <wilksonflor12@gmail.com>
2023-08-04 19:42:38 -0300

    fazer a rota para login e cadastro

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -23,26 +23,27 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const credenciais = {
-      usuario,
-      password,
-    };
-    axios
-    .post("http://localhost:5000/authenticate", credenciais)
-    .then((response) => {
-      // Verifique a resposta do servidor
-      if (response.data.success) {
-        // Se a autenticação for bem-sucedida, redirecione para a página inicial
-        navigate("/home");
-      } else {
-        // Se a autenticação falhar, exiba uma mensagem de erro ou tome outra ação adequada
-        console.error("Credenciais inválidas");
-      }
-    })
-    .catch((error) => {
-      // Em caso de erro na requisição
-      console.error("Erro ao fazer a autenticação:", error);
-    });
+    // const credenciais = {
+    //   usuario,
+    //   password,
+    // };
+    // axios
+    // .post("http://localhost:5000/authenticate", credenciais)
+    // .then((response) => {
+    //   // Verifique a resposta do servidor
+    //   if (response.data.success) {
+    //     // Se a autenticação for bem-sucedida, redirecione para a página inicial
+    //     navigate("/home");
+    //   } else {
+    //     // Se a autenticação falhar, exiba uma mensagem de erro ou tome outra ação adequada
+    //     console.error("Credenciais inválidas");
+    //   }
+    // })
+    // .catch((error) => {
+    //   // Em caso de erro na requisição
+    //   console.error("Erro ao fazer a autenticação:", error);
+    // });
+    navigate('/home')
   };
 
   return (
