b8b53a2f6863dc71db45fdefbee91c9f2c27c58a
Wilkson Flor <wilksonflor12@gmail.com>
2023-10-23 19:46:26 -0300

    vite para deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // resolve: {
-  //   alias: {
-  //     'pdfmake/build/vfs_fonts': '/home/ec2-user/app/gerenciadorCcr/frontend/node_modules/pdfmake/build/vfs_fonts',
-  //   },
-  // },
+  resolve: {
+    alias: {
+      'pdfmake/build/vfs_fonts': '/home/ec2-user/app/gerenciadorCcr/frontend/node_modules/pdfmake/build/vfs_fonts',
+    },
+  },
 })
 
