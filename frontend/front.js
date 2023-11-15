f2038b931d0fa58103dbccf6f39991cc296f11bf
Wilkson Flor <wilksonflor12@gmail.com>
2023-11-15 14:16:21 -0300

    front.js

--- frontend/front.js
+++ frontend/front.js
@@ -5,6 +5,6 @@ var serveStatic = require('serve-static');
 var app = express();
 app.use(serveStatic(path.join( './dist')));
 
-var port = 5475;
+var port = 5173;
 app.listen(port);
 console.log(`Server started ${port}`);
